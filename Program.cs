5370910c56387b8ea2249b19e49f088af55ca4c7
raquel <m-raquel@outlook.com.br>
2022-12-28 21:12:40 -0300

    changes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@ builder.Services.AddDbContext<BancoDeDados>(
     options => options.UseSqlite("Data Source=BancoDeDados.db")
 );
 
+builder.Services.AddCors(options => options.AddDefaultPolicy((builder) => {
+ builder.AllowAnyOrigin().AllowAnyHeader();
+}));
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapPost("/Colaborador/", (BancoDeDados bd, Colaborador colaborador) =>
 {   
@@ -13,6 +19,16 @@ app.MapPost("/Colaborador/", (BancoDeDados bd, Colaborador colaborador) =>
     bd.SaveChangesAsync();
     return Results.Ok(colaborador);
 });
+
+app.MapGet("/Colaborador/", async (BancoDeDados bd) => {
+    var colaboradores = await bd.colaborador.ToListAsync();
+    return Results.Ok(colaboradores);
+} );
+
+app.MapGet("/Colaborador/{id}", async (BancoDeDados bd, string  id, Colaborador colaborador) => {
+    var colaborador1 = await bd.colaborador.FirstOrDefaultAsync(id);
+    return Results.Ok(colaborador1);
+} );
 
 app.MapPut("/Colaborador/{id}", (BancoDeDados bd, int id, Colaborador colaborador) => 
 {
